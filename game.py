866e18f46d18fea5d3f195c0e11d03eba68bcd35
KAPOKA00 <kwakjm613@naver.com>
2023-05-02 14:35:18 +0900

    feat: process()
    함수 프로세스를 작성 했습니다.

--- game.py
+++ game.py
@@ -8,6 +8,21 @@ def Random_Input():
 
 def Process():
  print("처리")
+ import random
+
+ random_list = [1, 2, 4, 3]
+ user_num = [0, 1, 7 , 3]
+
+ bull_num = 0
+ cow_num = 0
+ for i in range(0, 4):
+     if random_list.count(user_num[i]):
+         cow_num += 1
+
+     if random_list[i] == user_num[i]:
+         bull_num += 1
+ print('cow_num = ',cow_num)
+ print('bull_num = ',bull_num)
 
 if __name__ == "__main__":
  User_Input()
