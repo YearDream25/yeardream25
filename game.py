8e03ab4bb1f42207c26521e2b2b271f156e4f667
GoX2Maker <gox2maker@gmail.com>
2023-05-02 14:03:19 +0900

    feat : Add game.py
    기본 틀은 만들었습니다.

---
+++ game.py
@@ -0,0 +1,17 @@
+def User_Input():
+ #사용자 입력
+ print("사용자 입력")
+
+
+def Random_Input():
+ print("랜덤")
+
+def Process():
+ print("처리")
+
+if __name__ == "__main__":
+ User_Input()
+ Random_Input()
+ Process()
+
+
