ffd7c2a0169f7ce964bef7b72b8dff4cb1cbbd93
juyony <wnsdyd1991@gamil.com>
2023-05-02 15:51:20 +0900

    feat: create gaeme.py for run game

--- game.py
+++ game.py
@@ -7,7 +7,20 @@ def Random_Input():
  print("랜덤")
 
 def Process():
- print("처리")
+    while (not end) and (count != 10):
+        display = []
+        for position in range(4):
+            user_choice = int(input(" "))
+            if com_choice.count(user_choice) > 0 and (com_choice[position] == user_choice):
+                display.append("B")
+            elif com_choice.count(user_choice) > 0:
+                display.append("C")
+        count += 1
+
+        print(display)
+        print("B",display.count("B"), "\nC",display.count("C"))
+        if display == ['B', 'B', 'B', 'B']:
+            end = True
 
 if __name__ == "__main__":
  User_Input()
