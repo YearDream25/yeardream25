2d790c6e2d3e36d4b9141421e29aa47293f9afcb
hyexk <jdh5060@gmail.com>
2023-05-02 16:16:10 +0900

    feat: Create process by Donghyeok

--- game.py
+++ game.py
@@ -6,7 +6,29 @@ def Random_Input():
  print("랜덤")
 
 def Process():
- print("처리")
+num1 = (4, 3, 9 ,8)
+num2 = (4, 3, 9, 8)
+
+result = 0
+for i in range(4):
+    if num1[i] == num2[i]:
+        result += 1
+    else:
+        continue
+if result == 4:
+    print("정답!")
+else:
+    print("Bulls : ", result)
+
+result2 = 0
+for i in range(4):
+    if num2[i] in num1:
+        result2 += 1
+    else:
+        continue
+print("Cows : ", result2-result)
+
+
 
 if __name__ == "__main__":
  User_Input()
